04c1d987477b43d698d62fafb20196734826ceb6
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-27 20:03:23 -0800

    Started comment.js model

---
+++ models/Comment.js
@@ -0,0 +1,43 @@
+const {Model, DataTypes} = require(`sequelize`);
+const sequelize = require(`../config/connection`);
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    text: {
+        type: DataTypes.TEXT,
+        allowNull:false,
+      
+    },
+    article_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: `article`,
+            key: `id`
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model:`user`,
+            key: `id`
+        }
+    }
+},
+{
+    sequelize,
+    freezeTableName:true,
+    underscored:true,
+    timestamps:true,
+    modelName:`comment`
+});
+
+module.exports = Comment;
