110193e0b23e0bb8d8f21a821ad3382fbf66cdc8
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-27 19:44:22 -0800

    started views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,32 @@
 const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+router.get('/', withAuth, async (req, res) => { //need to be authorized that log in is true
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true })); //remove special characters
+
+    res.render('homepage', { //then it will render the homepage
+      users,
+      logged_in: req.session.logged_in, //will this render your session data?
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session'); //without database linked on line 9
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store); //object returned by the require did not have session.store until we passed it. We are passing a paramater to connect-sessoion-sequelize
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Create the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers });
 
-// Inform Express.js which template engine we're using
+const sess = { //configuring how the sessions will be created
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ //telling the session library where to save session details permanently - in sequelize
+    db: sequelize
+  })
+};
+
+//session is data in server-side and cookies are data in the browser
+//session is an object that stores details about the user experience - stored in the server
+//cookies is just a file stored and managed by the browser - key value pairs inside. key name - value Mariah, DOB key - value 1993. Cookies are created by the server and kept by the browser
+
+//Together they work. Inside the cookie is the session and the session ID
+//Every request from browser to the server will go have the cookie and the cookie will contain the session data and the server responds accordinly.
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
