82d414b65cc82565cd7e57776b6aed772f7b1ab5
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-05 14:41:55 -0800

    not finished seed.js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,9 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
+const blogData = require('./blogPosts.json');
+const commentData = require('./comments.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +12,8 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+  // Need to seed blogs and comments NOT DONE
 
   process.exit(0);
 };
